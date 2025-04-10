2e00b607faf5f31ff3588ce96d08a9db31f770f8
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-09 23:07:19 -0300

    init: spring Initializr

---
+++ src/test/java/com/victorsaraiva/auth_base_jwt/AuthBaseJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorsaraiva.auth_base_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthBaseJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
