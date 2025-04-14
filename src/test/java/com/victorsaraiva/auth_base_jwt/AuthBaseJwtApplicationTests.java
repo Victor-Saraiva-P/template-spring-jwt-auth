9592d9f71e6ace79d93b5fe6b7347f1f4e2b376e
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-14 15:10:50 -0300

    docs: adiciona comentários explicativos no teste de carregamento do contexto da aplicação

--- src/test/java/com/victorsaraiva/auth_base_jwt/AuthBaseJwtApplicationTests.java
+++ src/test/java/com/victorsaraiva/auth_base_jwt/AuthBaseJwtApplicationTests.java
@@ -8,6 +8,8 @@ class AuthBaseJwtApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Este metodo está vazio propositalmente, pois verifica apenas
+		// se o contexto da aplicação Spring Boot é carregado sem erros
 	}
 
 }
