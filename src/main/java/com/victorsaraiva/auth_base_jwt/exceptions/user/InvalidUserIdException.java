2eef5167a9a43d84c210aafb430a3d4433655a3e
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-14 17:23:20 -0300

    remove: remove exceptions não usadas

--- src/main/java/com/victorsaraiva/auth_base_jwt/exceptions/user/InvalidUserIdException.java
+++
@@ -1,7 +0,0 @@
-package com.victorsaraiva.auth_base_jwt.exceptions.user;
-
-public class InvalidUserIdException extends RuntimeException {
-    public InvalidUserIdException() {
-        super("O userId não pode ser nulo ou vazio");
-    }
-}
--- src/main/java/com/victorsaraiva/auth_base_jwt/exceptions/user/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.victorsaraiva.auth_base_jwt.exceptions.user;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String uuid) {
-        super("Usuário com UUID " + uuid + " não encontrada");
-    }
-}
