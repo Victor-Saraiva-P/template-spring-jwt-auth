dfad5049448abd1b9df8b9ddf6d0a1aaf20eb67f
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-16 14:31:28 -0300

    fix: corrige tipo de retorno do método findByUsername no UserRepository

--- src/main/java/com/victorsaraiva/auth_base_jwt/repositories/UserRepository.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/repositories/UserRepository.java
@@ -11,5 +11,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     Optional<UserEntity> findByEmail(String email);
 
-    Optional<Object> findByUsername(String username);
+    Optional<UserEntity> findByUsername(String username);
 }
