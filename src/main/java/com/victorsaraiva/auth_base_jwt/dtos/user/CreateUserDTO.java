c73f0dc02abe7218c0ee53adbbf1dbfd9be97260
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-16 14:30:55 -0300

    refactor: remove getter annotation from CreateUserDTO to streamline code
    remove: remove Getter annotation redundante por conta do @Data em CreateUserDTO

--- src/main/java/com/victorsaraiva/auth_base_jwt/dtos/user/CreateUserDTO.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/dtos/user/CreateUserDTO.java
@@ -5,13 +5,15 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
 public class CreateUserDTO {
 
     @NotBlank(message = "O username é obrigatório")
