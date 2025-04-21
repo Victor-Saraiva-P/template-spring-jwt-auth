c169d6f5f57057c6419e207c2b64b9611bbc161d
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-21 15:58:46 -0300

    remove: remove arquivos criados anteriormente para um novo começo

--- src/main/java/com/victorsaraiva/auth_base_jwt/exceptions/user/InvalidPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.victorsaraiva.auth_base_jwt.exceptions.user;
-
-public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException() {
-        super("Senha inválida");
-    }
-}
--- src/main/java/com/victorsaraiva/auth_base_jwt/models/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.victorsaraiva.auth_base_jwt.models.enums;
-
-public enum Role {
-    USER,
-    ADMIN,
-}
