62814dad47182e55e107e5d2ca67a002ef4e631f
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-28 14:17:19 -0300

    style: ajusta a formatação do método de teste para melhor legibilidade

--- src/test/java/com/victorsaraiva/auth_base_jwt/AuthBaseJwtApplicationTests.java
+++ src/test/java/com/victorsaraiva/auth_base_jwt/AuthBaseJwtApplicationTests.java
@@ -8,10 +8,9 @@ import org.springframework.test.context.ActiveProfiles;
 @ActiveProfiles("test")
 class AuthBaseJwtApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // Este metodo está vazio propositalmente, pois verifica apenas
-        // se o contexto da aplicação Spring Boot é carregado sem erros
-    }
+  @Test
+  void contextLoads() {
+    // Este metodo está vazio propositalmente, pois verifica apenas
+    // se o contexto da aplicação Spring Boot é carregado sem erros
+  }
 }
-
