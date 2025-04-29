6349f2ae5b544427129e2e05d9d7629c22b05b36
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-29 12:26:12 -0300

    style: aplicação do spotless

--- src/test/java/com/victorsaraiva/auth_base_jwt/configs/PasswordConfigTest.java
+++ src/test/java/com/victorsaraiva/auth_base_jwt/configs/PasswordConfigTest.java
@@ -1,44 +1,43 @@
 package com.victorsaraiva.auth_base_jwt.configs;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 public class PasswordConfigTest {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+  @Autowired private PasswordEncoder passwordEncoder;
 
-    @Test
-    public void testPasswordEncoder() {
-        // Arrange
-        String senhaOriginal = "minhasenha123";
+  @Test
+  public void testPasswordEncoder() {
+    // Arrange
+    String senhaOriginal = "minhasenha123";
 
-        // Act
-        String senhaEncodada = passwordEncoder.encode(senhaOriginal);
+    // Act
+    String senhaEncodada = passwordEncoder.encode(senhaOriginal);
 
-        // Assert
-        assertNotNull(senhaEncodada);
-        assertNotEquals(senhaOriginal, senhaEncodada);
-        assertTrue(passwordEncoder.matches(senhaOriginal, senhaEncodada));
-    }
+    // Assert
+    assertNotNull(senhaEncodada);
+    assertNotEquals(senhaOriginal, senhaEncodada);
+    assertTrue(passwordEncoder.matches(senhaOriginal, senhaEncodada));
+  }
 
-    @Test
-    public void testSenhasIguaisComEncodingsDiferentes() {
-        // Arrange
-        String senha = "minhasenha123";
+  @Test
+  public void testSenhasIguaisComEncodingsDiferentes() {
+    // Arrange
+    String senha = "minhasenha123";
 
-        // Act
-        String primeiroEncoding = passwordEncoder.encode(senha);
-        String segundoEncoding = passwordEncoder.encode(senha);
+    // Act
+    String primeiroEncoding = passwordEncoder.encode(senha);
+    String segundoEncoding = passwordEncoder.encode(senha);
 
-        // Assert
-        assertNotEquals(primeiroEncoding, segundoEncoding);
-        assertTrue(passwordEncoder.matches(senha, primeiroEncoding));
-        assertTrue(passwordEncoder.matches(senha, segundoEncoding));
-    }
+    // Assert
+    assertNotEquals(primeiroEncoding, segundoEncoding);
+    assertTrue(passwordEncoder.matches(senha, primeiroEncoding));
+    assertTrue(passwordEncoder.matches(senha, segundoEncoding));
+  }
 }
