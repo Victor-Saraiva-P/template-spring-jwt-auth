3f978a7e4e06245323e2ea52887b7b477feeb9cc
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-29 19:40:55 -0300

    feat: adiciona configuração de segurança com filtro JWT e gerenciamento de autenticação

---
+++ src/main/java/com/victorsaraiva/auth_base_jwt/security/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.victorsaraiva.auth_base_jwt.security;
+
+import com.victorsaraiva.auth_base_jwt.configs.PasswordConfig;
+import com.victorsaraiva.auth_base_jwt.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+  @Value("${api.base-url}")
+  private String apiBasePath;
+
+  public final JwtAuthFilter jwtAuthFilter;
+  public final UserDetailsServiceImpl userDetailsService;
+  public final PasswordConfig passwordConfig;
+
+  public SecurityConfig(
+      JwtAuthFilter jwtAuthFilter,
+      UserDetailsServiceImpl userDetailsService,
+      PasswordConfig passwordConfig) {
+    this.jwtAuthFilter = jwtAuthFilter;
+    this.userDetailsService = userDetailsService;
+    this.passwordConfig = passwordConfig;
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers(apiBasePath + "/auth/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider())
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(userDetailsService);
+    authenticationProvider.setPasswordEncoder(passwordConfig.passwordEncoder());
+    return authenticationProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+      throws Exception {
+    return config.getAuthenticationManager();
+  }
+}
