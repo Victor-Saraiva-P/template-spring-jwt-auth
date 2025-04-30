1cbbfa247c06d9c4f67d604e3c6e3cd564fd578b
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-30 14:57:56 -0300

    feat: adiciona exceções personalizadas para operações de usuário

---
+++ src/main/java/com/victorsaraiva/auth_base_jwt/exceptions/user/EmailNotFoundException.java
@@ -0,0 +1,7 @@
+package com.victorsaraiva.auth_base_jwt.exceptions.user;
+
+public class EmailNotFoundException extends RuntimeException {
+  public EmailNotFoundException(String email) {
+    super("Email " + email + " não encontrado");
+  }
+}
---
+++ src/main/java/com/victorsaraiva/auth_base_jwt/exceptions/user/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.victorsaraiva.auth_base_jwt.exceptions.user;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+  public UsernameAlreadyExistsException(String username) {
+    super("Username " + username + " já cadastrado");
+  }
+}
