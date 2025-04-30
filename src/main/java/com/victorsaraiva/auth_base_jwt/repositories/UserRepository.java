830d512bdb469f810f1354e9a057d6bac2a1e30e
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-30 10:39:11 -0300

    refactor: renomeia o parametro de findByEmail para deixar mais claro que é email

--- src/main/java/com/victorsaraiva/auth_base_jwt/repositories/UserRepository.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/repositories/UserRepository.java
@@ -1,10 +1,11 @@
 package com.victorsaraiva.auth_base_jwt.repositories;
 
 import com.victorsaraiva.auth_base_jwt.models.UserEntity;
-import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-  Optional<UserEntity> findByEmail(String login);
+  Optional<UserEntity> findByEmail(String email);
 }
