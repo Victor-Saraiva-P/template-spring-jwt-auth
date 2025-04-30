33c77be66aedd8f7636de2d435c224af5c21287e
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-30 14:58:11 -0300

    feat: implementa AuthController com endpoints para registro e login de usuários

---
+++ src/main/java/com/victorsaraiva/auth_base_jwt/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.victorsaraiva.auth_base_jwt.controller;
+
+import com.victorsaraiva.auth_base_jwt.dtos.user.CreateUserDTO;
+import com.victorsaraiva.auth_base_jwt.dtos.user.LoginUserDTO;
+import com.victorsaraiva.auth_base_jwt.dtos.user.UserDTO;
+import com.victorsaraiva.auth_base_jwt.models.UserEntity;
+import com.victorsaraiva.auth_base_jwt.services.AuthService;
+import com.victorsaraiva.auth_base_jwt.services.JwtService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("${api.base-url}/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    private final JwtService jwtService;
+
+    public AuthController(AuthService authService, JwtService jwtService) {
+        this.authService = authService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> register(@Valid @RequestBody CreateUserDTO createUserDTO) {
+        UserDTO registeredUser = this.authService.register(createUserDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@Valid @RequestBody LoginUserDTO loginUserDTO) {
+        UserEntity userEntity = authService.login(loginUserDTO);
+
+        // Gera o token JWT
+        String acessToken = jwtService.generateToken(userEntity);
+
+        return ResponseEntity.ok(Map.of("acessToken", acessToken));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/ping")
+    public String test() {
+        return "Olá mundo";
+    }
+}
