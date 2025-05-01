d304b066af5d6d616515670e2010bde1a2511942
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-01 11:17:00 -0300

    remove: remove logger que não são mais necessarios para debug

--- src/main/java/com/victorsaraiva/auth_base_jwt/services/UserDetailsServiceImpl.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/services/UserDetailsServiceImpl.java
@@ -3,8 +3,6 @@ package com.victorsaraiva.auth_base_jwt.services;
 import com.victorsaraiva.auth_base_jwt.models.UserEntity;
 import com.victorsaraiva.auth_base_jwt.repositories.UserRepository;
 import com.victorsaraiva.auth_base_jwt.security.CustomUserDetails;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,24 +12,16 @@ import org.springframework.stereotype.Component;
 public class UserDetailsServiceImpl implements UserDetailsService {
   private final UserRepository userRepository;
 
-  private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
-
   public UserDetailsServiceImpl(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    logger.debug("Procurando usuário pelo identificador: {}", email);
     UserEntity userEntityEncontrado =
         userRepository
             .findByEmail(email) // busca o usuário pelo e-mail
-            .orElseThrow(
-                () -> {
-                  logger.error("E-mail não encontrado: {}", email);
-                  return new UsernameNotFoundException("Usuário não encontrado");
-                });
-    logger.info("Usuário encontrado: {}", userEntityEncontrado.getEmail());
+            .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
     return new CustomUserDetails(userEntityEncontrado);
   }
 }
