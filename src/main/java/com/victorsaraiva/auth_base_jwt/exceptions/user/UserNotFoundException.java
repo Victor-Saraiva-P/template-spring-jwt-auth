ff59e32d935b607aa0da89898fe1eac5002d28e5
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-02 20:27:42 -0300

    remove: remove exception não utilizada

--- src/main/java/com/victorsaraiva/auth_base_jwt/exceptions/user/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.victorsaraiva.auth_base_jwt.exceptions.user;
-
-public class UserNotFoundException extends RuntimeException {
-  public UserNotFoundException(String email) {
-    super("Usuário com email '" + email + "' não encontrado");
-  }
-}
