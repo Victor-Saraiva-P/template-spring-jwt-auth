0ca413da6736f8fc31bd5a1d1b44e17902a28f48
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-05 15:42:47 -0300

    feat: adiciona Lombok @RequiredArgsConstructor para fazer a injeção de construtor

--- src/main/java/com/victorsaraiva/auth_base_jwt/controller/AuthController.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/controller/AuthController.java
@@ -11,6 +11,7 @@ import com.victorsaraiva.auth_base_jwt.services.AccessTokenService;
 import com.victorsaraiva.auth_base_jwt.services.AuthService;
 import com.victorsaraiva.auth_base_jwt.services.RefreshTokenService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("${api.base-url}/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
   private final AuthService authService;
