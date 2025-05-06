6794cf896a494515d4290665eb121cb56335dd1f
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-06 14:02:05 -0300

    feat: atualiza o metodo de logout para incluir o user e melhora a deleção do refresh token

--- src/main/java/com/victorsaraiva/auth_base_jwt/controller/AuthController.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/controller/AuthController.java
@@ -8,16 +8,17 @@ import com.victorsaraiva.auth_base_jwt.dtos.jwt.RefreshTokenDTO;
 import com.victorsaraiva.auth_base_jwt.dtos.user.UserResponseDTO;
 import com.victorsaraiva.auth_base_jwt.models.RefreshTokenEntity;
 import com.victorsaraiva.auth_base_jwt.models.UserEntity;
+import com.victorsaraiva.auth_base_jwt.security.CustomUserDetails;
 import com.victorsaraiva.auth_base_jwt.services.AccessTokenService;
 import com.victorsaraiva.auth_base_jwt.services.AuthService;
 import com.victorsaraiva.auth_base_jwt.services.BlacklistService;
 import com.victorsaraiva.auth_base_jwt.services.RefreshTokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -26,9 +27,6 @@ import java.util.Date;
 @RequestMapping("${api.base-url}/auth")
 @RequiredArgsConstructor
 public class AuthController {
-
-  @Value("${security.refresh-token.expiration}")
-  private long REFRESH_TOKEN_EXPIRATION; // em milissegundos
 
   private final AuthService authService;
   private final AccessTokenService accessTokenService;
@@ -77,9 +75,11 @@ public class AuthController {
   public ResponseEntity<Void> logout(
       @RequestHeader("Authorization") String authHeader,
       @CookieValue("refreshToken") String refreshToken,
-      @CookieValue("refreshTokenId") Long refreshTokenId) {
+      @CookieValue("refreshTokenId") Long refreshTokenId,
+      @AuthenticationPrincipal CustomUserDetails userDetails) {
 
     String accessToken = authHeader.replace("Bearer ", "");
+    UserEntity loggedUser = userDetails.user();
 
     // Extrai claims do access token
     String jti = accessTokenService.extractId(accessToken);
@@ -89,7 +89,7 @@ public class AuthController {
     blacklistService.blacklist(jti, exp.toInstant());
 
     // Deleta o refresh token
-    refreshTokenService.deleteById(refreshTokenId);
+    refreshTokenService.deleteRefreshToken(refreshToken, refreshTokenId, loggedUser);
     return ResponseEntity.noContent().build();
   }
 
