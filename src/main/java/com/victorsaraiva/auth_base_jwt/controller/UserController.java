953c406974a9d219b9dc125e306ca836b6f01dfe
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-07 13:30:26 -0300

    feat: adiciona método para deletar conta de usuário com verificação de autorização

--- src/main/java/com/victorsaraiva/auth_base_jwt/controller/UserController.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/controller/UserController.java
@@ -2,12 +2,19 @@ package com.victorsaraiva.auth_base_jwt.controller;
 
 import com.victorsaraiva.auth_base_jwt.dtos.user.ChangeRoleRequestDTO;
 import com.victorsaraiva.auth_base_jwt.dtos.user.UserDTO;
+import com.victorsaraiva.auth_base_jwt.exceptions.user.UnauthorizedAccountDeletionException;
+import com.victorsaraiva.auth_base_jwt.models.UserEntity;
+import com.victorsaraiva.auth_base_jwt.security.CustomUserDetails;
 import com.victorsaraiva.auth_base_jwt.services.UserService;
-import java.util.List;
-import java.util.UUID;
+import com.victorsaraiva.auth_base_jwt.services.security.AccessTokenService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("${api.base-url}/users")
@@ -15,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
   private final UserService userService;
+  private final AccessTokenService accessTokenService;
 
   @GetMapping
   public ResponseEntity<List<UserDTO>> getAllUsers() {
@@ -23,8 +31,26 @@ public class UserController {
 
   @PatchMapping("/change-role/{userId}")
   public ResponseEntity<Void> changeUserRole(
-      @PathVariable UUID userId, @RequestBody ChangeRoleRequestDTO changeRoleRequestDTO) {
-    userService.changeUserRole(changeRoleRequestDTO, userId);
+      @PathVariable UUID userId, @Valid @RequestBody ChangeRoleRequestDTO changeRoleRequest) {
+
+    userService.changeUserRole(changeRoleRequest, userId);
+    return ResponseEntity.noContent().build();
+  }
+
+  @DeleteMapping("/delete-your-account")
+  public ResponseEntity<Void> deleteUser(
+      @RequestHeader("Authorization") String authHeader,
+      @AuthenticationPrincipal CustomUserDetails userDetails) {
+
+    String accessToken = authHeader.replace("Bearer ", "");
+    String userIdFromAccessToken = accessTokenService.extractSubject(accessToken);
+    UserEntity loggedUser = userDetails.user();
+
+    if (!userIdFromAccessToken.equals(loggedUser.getId().toString())) {
+      throw new UnauthorizedAccountDeletionException();
+    }
+
+    userService.deleteUser(loggedUser.getId());
     return ResponseEntity.noContent().build();
   }
 }
