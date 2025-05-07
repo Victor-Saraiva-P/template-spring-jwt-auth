7ad18b72de9c8638a9bc58b3e9421fa1750e3a00
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-07 13:12:24 -0300

    feat: adiciona método para deletar todos os refresh tokens por ID de usuário

--- src/main/java/com/victorsaraiva/auth_base_jwt/repositories/RefreshTokenRepository.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/repositories/RefreshTokenRepository.java
@@ -1,9 +1,13 @@
 package com.victorsaraiva.auth_base_jwt.repositories;
 
 import com.victorsaraiva.auth_base_jwt.models.RefreshTokenEntity;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
   Optional<RefreshTokenEntity> findByToken(String token);
+
+    void deleteAllByUserId(UUID userId);
 }
