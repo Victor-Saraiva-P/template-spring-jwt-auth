703ceb0065f77f798d857a53d0baabac69f5ef33
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-06 21:35:05 -0300

    feat: adiciona método para alterar o papel de usuário no UserService

--- src/main/java/com/victorsaraiva/auth_base_jwt/services/UserService.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/services/UserService.java
@@ -1,12 +1,16 @@
 package com.victorsaraiva.auth_base_jwt.services;
 
+import com.victorsaraiva.auth_base_jwt.dtos.user.ChangeRoleRequestDTO;
 import com.victorsaraiva.auth_base_jwt.dtos.user.UserDTO;
+import com.victorsaraiva.auth_base_jwt.exceptions.user.UserNotFoundException;
 import com.victorsaraiva.auth_base_jwt.mappers.Mapper;
 import com.victorsaraiva.auth_base_jwt.models.UserEntity;
 import com.victorsaraiva.auth_base_jwt.repositories.UserRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,5 +23,14 @@ public class UserService {
     List<UserEntity> userEntities = userRepository.findAll();
 
     return userEntities.stream().map(userMapper::mapTo).toList();
+  }
+
+  public UserDTO changeUserRole(ChangeRoleRequestDTO changeRoleRequestDTO, UUID userId) {
+    UserEntity user =
+        userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+
+    user.setRole(changeRoleRequestDTO.getRoleEnum());
+    userRepository.save(user);
+    return userMapper.mapTo(user);
   }
 }
