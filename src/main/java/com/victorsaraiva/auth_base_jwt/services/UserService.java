254d2df6403eee311c5f2f48dd4e134c8bd3970f
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-07 13:12:37 -0300

    feat: adiciona método para deletar usuário e seus refresh tokens

--- src/main/java/com/victorsaraiva/auth_base_jwt/services/UserService.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/services/UserService.java
@@ -5,7 +5,9 @@ import com.victorsaraiva.auth_base_jwt.dtos.user.UserDTO;
 import com.victorsaraiva.auth_base_jwt.exceptions.user.UserNotFoundException;
 import com.victorsaraiva.auth_base_jwt.mappers.Mapper;
 import com.victorsaraiva.auth_base_jwt.models.UserEntity;
+import com.victorsaraiva.auth_base_jwt.repositories.RefreshTokenRepository;
 import com.victorsaraiva.auth_base_jwt.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,7 @@ import java.util.UUID;
 public class UserService {
 
   private final UserRepository userRepository;
+  private final RefreshTokenRepository refreshTokenRepository;
   private final Mapper<UserEntity, UserDTO> userMapper;
 
   public List<UserDTO> getAllUsers() {
@@ -27,10 +30,22 @@ public class UserService {
 
   public void changeUserRole(ChangeRoleRequestDTO changeRoleRequestDTO, UUID userId) {
     UserEntity user =
-        userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+      userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
 
     user.setRole(changeRoleRequestDTO.getRoleEnum());
     userRepository.save(user);
     userMapper.mapTo(user);
+  }
+
+  @Transactional
+  public void deleteUser(UUID userId) {
+    UserEntity user =
+      userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+
+    // Remove todos os refresh tokens do usuario
+    refreshTokenRepository.deleteAllByUserId(userId);
+
+    // Remove o usuario
+    userRepository.delete(user);
   }
 }
