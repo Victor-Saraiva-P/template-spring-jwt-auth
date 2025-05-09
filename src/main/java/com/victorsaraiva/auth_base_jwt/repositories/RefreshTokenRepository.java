b09ccae685d621819764825437d212722109e5f1
Victor Saraiva <victor_alex2005@hotmail.com>
2025-05-09 18:31:30 -0300

    remove: remove metodo não usado findByToken do RefreshTokenRepository

--- src/main/java/com/victorsaraiva/auth_base_jwt/repositories/RefreshTokenRepository.java
+++ src/main/java/com/victorsaraiva/auth_base_jwt/repositories/RefreshTokenRepository.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshTokenEntity, Long> {
-  Optional<RefreshTokenEntity> findByToken(String token);
 
   void deleteAllByUserId(UUID userId);
 
